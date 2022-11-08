6c83a427731fa44de2c65abeb0613d8ad452b77a
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-11-08 12:10:12 +0800

    Time: 57 ms (52.05%), Space: 14 MB (31.83%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        digToChart = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+        def dfs(i, sub):
+            if i >= len(digits):
+                res.append(sub)
+                return
+            for c in digToChart[digits[i]]:
+                dfs(i + 1, sub + c)
+            
+        if digits:
+            dfs(0, "")
+        
+        return res
+        
