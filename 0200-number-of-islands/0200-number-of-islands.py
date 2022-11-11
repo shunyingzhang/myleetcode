464b54d4326c5b39097a4f1f9b2fa5f0b2aff747
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-11-11 09:18:14 +0800

    Time: 429 ms (80.25%), Space: 21.9 MB (19.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,5 +1,5 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self, grid: List[List[int]]) -> int:
         if not grid:
             return 0
         
