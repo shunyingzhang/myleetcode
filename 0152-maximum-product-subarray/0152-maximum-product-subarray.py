e93e01d4abbacc633daed732a30abf52f03d307e
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-11-17 09:31:01 +0800

    Time: 211 ms (26.74%), Space: 14.4 MB (80.95%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -3,19 +3,21 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         
-        maxMin = {i : [] for i in range(len(nums))}
-        
-        maxMin[0].append(nums[0])
-        maxMin[0].append(nums[0])
+        curMax = 1
+        curMin = 1
         
         res = nums[0]
         
-        for i in range(1, len(nums)):
-            maxMin[i].append(max(nums[i], 
-                                 nums[i] * maxMin[i - 1][0], 
-                                 nums[i] * maxMin[i - 1][1]))
-            res = max(res, maxMin[i][0])
-            maxMin[i].append(min(nums[i], nums[i] * maxMin[i - 1][0], nums[i] * maxMin[i - 1][1]))
+        for c in nums:
+            tmp = curMax
+            curMax = max(c, c*curMax, c*curMin)
+            curMin = min(c, c*tmp, c*curMin)
+            res = max(res, curMax)
+            # maxMin[i].append(max(nums[i], 
+            #                      nums[i] * maxMin[i - 1][0], 
+            #                      nums[i] * maxMin[i - 1][1]))
+            # res = max(res, maxMin[i][0])
+            # maxMin[i].append(min(nums[i], nums[i] * maxMin[i - 1][0], nums[i] * maxMin[i - 1][1]))
         
         return res
         
