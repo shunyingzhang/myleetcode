0d6ad79ac1eac30546b03227f3c72cf5e1b79f31
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-11-23 10:17:54 +0800

    Time: 68 ms (19.77%), Space: 14.7 MB (9.98%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,22 +1,45 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
-        lMax = 0
-        lMin = 0
+        dp = {}
         
-        for c in s:
-            if c == '(':
-                lMax += 1
-                lMin += 1
-            elif c == '*':
-                lMax += 1
-                lMin -= 1
-            else:
-                lMax -= 1
-                lMin -= 1
-            if lMax < 0:
+        def dfs(i, left):
+            if i == len(s):
+                return left == 0
+            if left < 0:
                 return False
-            if lMin < 0:
-                lMin = 0
+            if (i, left) in dp:
+                return dp[(i, left)]
+            
+            if s[i] == '(':
+                dp[(i, left)] = dfs(i + 1, left + 1)
+            if s[i] == ')':
+                dp[(i, left)] = dfs(i + 1, left - 1)
+            if s[i] == '*':
+                dp[(i, left)] = (dfs(i + 1, left + 1) or dfs(i + 1, left - 1) 
+                or dfs(i + 1, left))
+            return dp[(i, left)]
+                      
+                      
+        return dfs(0, 0)
         
-        return lMax >= 0 and lMin<= 0
+        
+#         lMax = 0
+#         lMin = 0
+        
+#         for c in s:
+#             if c == '(':
+#                 lMax += 1
+#                 lMin += 1
+#             elif c == '*':
+#                 lMax += 1
+#                 lMin -= 1
+#             else:
+#                 lMax -= 1
+#                 lMin -= 1
+#             if lMax < 0:
+#                 return False
+#             if lMin < 0:
+#                 lMin = 0
+        
+#         return lMin == 0
         
