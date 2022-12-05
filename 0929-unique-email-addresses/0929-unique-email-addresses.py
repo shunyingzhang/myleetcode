e9f5a0df306584fb506bff55caca75d43e237cab
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-05 15:07:09 +0800

    Time: 248 ms (5.11%), Space: 14 MB (74.52%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -3,19 +3,15 @@ class Solution:
         setE = set()
         for e in emails:
             tmp1 = ''
-            tmp2 = ''
-            plus = 0
-            for i, c in enumerate(e):
-                if c == '+':
-                    plus = 1
-                    continue
-                if c == '@':
-                    e = e[i + 1:]
-                    break
-                if plus != 1 and c != '.':
-                    tmp1 += c
-            for c in e:
-                tmp2 += c
+            i = 0
+            while e[i] not in ['@', '+']:
+                if e[i] != '.':
+                    tmp1 += e[i]
+                i += 1
+            while e[i] != '@':
+                i += 1
+            tmp2 = e[i + 1:]
+            
             setE.add((tmp1, tmp2))
         
         return len(setE)
