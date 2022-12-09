120bf3baddb688aae52c61944c41f564b7f36aa5
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-09 14:26:22 +0800

    Time: 68 ms (60.12%), Space: 14 MB (39.16%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,27 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1 = m - 1
-        p2 = n - 1
         p = m + n -1
         
-        while p1 >= 0 and p2 >= 0:
-            if nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[p] = nums1[m - 1]
+                m -= 1
                 p -= 1
-            elif nums1[p1] < nums2[p2]:
-                nums1[p] = nums2[p2]
-                p2 -= 1
+            elif nums1[m - 1] < nums2[n - 1]:
+                nums1[p] = nums2[n - 1]
+                n -= 1
                 p -= 1
             else:
-                nums1[p] = nums1[p1]
-                nums1[p - 1] = nums1[p1]
+                nums1[p] = nums1[m - 1]
+                nums1[p - 1] = nums1[m - 1]
                 p -= 2
-                p1 -= 1
-                p2 -= 1
-        if p2 >= 0:
-            for i in range(0, p2 + 1):
+                m -= 1
+                n -= 1
+        if n > 0:
+            for i in range(0, n):
                 nums1[i] = nums2[i]
                 
             
