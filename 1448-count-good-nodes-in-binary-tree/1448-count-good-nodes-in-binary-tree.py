b9686fa9928509a2f5b6d29d9d60b56ccbbb9a58
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-20 14:20:04 +1100

    Time: 734 ms (14.55%), Space: 32.5 MB (49.30%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -8,29 +8,51 @@ class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         if not root:
             return 0
+        gn = 0
         
-        GN = 0
+        def findG(node, ref):
+            nonlocal gn
+            if not node:
+                return 0
+            gn = findG(node.left, max(ref, node.val))
+            gn += findG(node.right, max(ref, node.val))
+            
+            if node.val >= ref:
+                return 1 + gn
+            else:
+                return gn
         
-        def findGoodN(p, value):
-            nonlocal GN
-            if p.val >= value:
-                value = p.val
-                GN += 1
-            if p.left:
-                findGoodN(p.left, value)
-            if p.right:
-                findGoodN(p.right, value)
-            return GN        
+        return findG(root, root.val)
         
-        res_val = root.val
         
-        left = 0
-        right = 0
         
-        if root.left:
-            GN = findGoodN(root.left, res_val)
-        if root.right:
-            GN = findGoodN(root.right, res_val)
+        
+        
+#         if not root:
+#             return 0
+        
+#         GN = 0
+        
+#         def findGoodN(p, value):
+#             nonlocal GN
+#             if p.val >= value:
+#                 value = p.val
+#                 GN += 1
+#             if p.left:
+#                 findGoodN(p.left, value)
+#             if p.right:
+#                 findGoodN(p.right, value)
+#             return GN        
+        
+#         res_val = root.val
+        
+#         left = 0
+#         right = 0
+        
+#         if root.left:
+#             GN = findGoodN(root.left, res_val)
+#         if root.right:
+#             GN = findGoodN(root.right, res_val)
             
         
-        return 1 + GN
+#         return 1 + GN
