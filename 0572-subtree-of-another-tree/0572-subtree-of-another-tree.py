425f5d8f015f935523cc7a7407ee241401daf73e
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-21 09:41:50 +1100

    Time: 126 ms (87.49%), Space: 14.6 MB (91.11%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,27 +6,52 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        
         def isSameTree(p, q):
             if not p and not q:
                 return True
             if not p or not q or p.val != q.val:
                 return False
+            return (isSameTree(p.left, q.left) and isSameTree(p.right, q.right))
         
-            return(isSameTree(p.left, q.left) and isSameTree(p.right, q.right))
-
-            
-        q = deque([root])
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
+        que = deque([root])
+        while que:
+            for i in range(len(que)):
+                node = que.popleft()
                 if node.val == subRoot.val:
-                    res = isSameTree(node, subRoot)
-                    if res:
+                    if isSameTree(node, subRoot):
                         return True
                 if node.left:
-                    q.append(node.left)
+                    que.append(node.left)
                 if node.right:
-                    q.append(node.right)
+                    que.append(node.right)
         return False
+        
+        
+        
+        
+        
+        
+        
+#         def isSameTree(p, q):
+#             if not p and not q:
+#                 return True
+#             if not p or not q or p.val != q.val:
+#                 return False
+        
+#             return(isSameTree(p.left, q.left) and isSameTree(p.right, q.right))
+
+            
+#         q = deque([root])
+#         while q:
+#             for i in range(len(q)):
+#                 node = q.popleft()
+#                 if node.val == subRoot.val:
+#                     res = isSameTree(node, subRoot)
+#                     if res:
+#                         return True
+#                 if node.left:
+#                     q.append(node.left)
+#                 if node.right:
+#                     q.append(node.right)
+#         return False
         
