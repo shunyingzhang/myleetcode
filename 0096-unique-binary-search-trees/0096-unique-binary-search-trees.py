3886cfae2a2dc0013306bec2d7f63eac9bd17de9
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-21 15:53:34 +1100

    Time: 29 ms (94.59%), Space: 13.9 MB (61.76%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,25 +1,40 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        hmap = {}
-        hmap[0] = 1
-        hmap[1] = 1
+        tnode = [1] * (n + 1)
         
-        def numT(n):
+        for nodes in range(2, n + 1):
             res = 0
+            for i in range(1, nodes + 1):
+                left = i - 1
+                right = nodes - i 
+                res += tnode[left] * tnode[right]
+            tnode[nodes] = res
+        return tnode[n]
+        
             
-            for i in range(1, n + 1):
-                if i - 1 in hmap:
-                    left = hmap[i - 1]
-                else:
-                    left = self.numTrees(i - 1)
-                    hmap[i - 1] = left
-                if n - i in hmap:
-                    right = hmap[n - i]
-                else:
-                    right = self.numTrees(n - i)
-                    hmap[n - i] = right
+        
+        
+        
+#         hmap = {}
+#         hmap[0] = 1
+#         hmap[1] = 1
+        
+#         def numT(n):
+#             res = 0
+            
+#             for i in range(1, n + 1):
+#                 if i - 1 in hmap:
+#                     left = hmap[i - 1]
+#                 else:
+#                     left = self.numTrees(i - 1)
+#                     hmap[i - 1] = left
+#                 if n - i in hmap:
+#                     right = hmap[n - i]
+#                 else:
+#                     right = self.numTrees(n - i)
+#                     hmap[n - i] = right
     
-                res += left * right             
-            return res
-        return numT(n)
+#                 res += left * right             
+#             return res
+#         return numT(n)
         
