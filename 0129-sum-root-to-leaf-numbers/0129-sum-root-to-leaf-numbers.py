0539ecd2696c9e1de75c8685e83cd2cab5f74ed4
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-22 08:28:03 +1100

    Time: 37 ms (82.88%), Space: 13.8 MB (97.87%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,13 +6,13 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        
-        def sumLf(node, num):
+        def sumleaf(node, num):
             if not node:
                 return 0
             num = num * 10 + node.val
             if not node.left and not node.right:
                 return num
-            return sumLf(node.left, num) + sumLf(node.right, num)
-        return sumLf(root, 0)
+            return sumleaf(node.left, num) + sumleaf(node.right, num)
+        return sumleaf(root, 0)
+            
         
