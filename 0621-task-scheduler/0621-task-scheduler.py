331a2d4e337eee99b84c511dd51ae34bc24c1d67
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-24 15:03:56 +1100

    Time: 708 ms (71.15%), Space: 14.2 MB (90.04%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,20 +1,46 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         count = Counter(tasks)
-        HeapMax = [-cnt for cnt in count.values()]
-        heapq.heapify(HeapMax)
-        
-        queue = deque()
+        heapmax = [-cnt for cnt in count.values()]
+        heapq.heapify(heapmax)
+        queue = []
         t = 0
-        
-        while HeapMax or queue:
+        while heapmax or queue:
             t += 1
-            if HeapMax:
-                cnt = 1 + heapq.heappop(HeapMax)
-                if cnt:
-                    queue.append([cnt, t + n])
+            if heapmax:
+                task = heapq.heappop(heapmax)
+                task += 1
+                if task != 0:
+                    queue.append([task, t + n])
+
+            while queue and queue[0][1] <= t:
+                heapq.heappush(heapmax, queue.pop()[0])
             
-            if queue and queue[0][1] == t:
-                heapq.heappush(HeapMax, queue.popleft()[0])
-        
         return t
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         count = Counter(tasks)
+#         HeapMax = [-cnt for cnt in count.values()]
+#         heapq.heapify(HeapMax)
+        
+#         queue = deque()
+#         t = 0
+        
+#         while HeapMax or queue:
+#             t += 1
+#             if HeapMax:
+#                 cnt = 1 + heapq.heappop(HeapMax)
+#                 if cnt:
+#                     queue.append([cnt, t + n])
+            
+#             if queue and queue[0][1] == t:
+#                 heapq.heappush(HeapMax, queue.popleft()[0])
+        
+#         return t
