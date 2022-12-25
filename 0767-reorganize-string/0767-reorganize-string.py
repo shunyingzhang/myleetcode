684fea09743ed9439daead0e6b1e795729272194
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-25 15:05:00 +1100

    Time: 44 ms (75.78%), Space: 13.9 MB (77.96%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -3,24 +3,37 @@ class Solution:
         count = Counter(s)
         heapmax = [[-cnt, key] for key, cnt in count.items()]
         heapq.heapify(heapmax)
-        waiting = []
+        waiting = None
         res = ''
         for i in range(len(s)):
-    
-            while waiting and waiting[0][0] <= i:
-                wait = heapq.heappop(waiting)
-                heapq.heappush(heapmax, [wait[1], wait[2]])
-                
-            if len(heapmax) == 0:
+            if waiting and not heapmax:
                 return ''
-    
+            
             count, key = heapq.heappop(heapmax)
-  
             res += key
             count += 1
+            
+            if waiting:
+                heapq.heappush(heapmax, waiting)
+                waiting = None
+            
             if count != 0:
-                heapq.heappush(waiting, [i + 2, count, key])
-
-        
+                waiting = [count, key]
+                
+    
+#             while waiting and waiting[0][0] <= i:
+#                 wait = heapq.heappop(waiting)
+#                 heapq.heappush(heapmax, [wait[1], wait[2]])
+                
+#             if len(heapmax) == 0:
+#                 return ''
+    
+#             count, key = heapq.heappop(heapmax)
+  
+#             res += key
+#             count += 1
+#             if count != 0:
+#                 heapq.heappush(waiting, [i + 2, count, key])
+                
         return res
         
