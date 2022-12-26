b1bfa3672bcc7531e031cd7427f61855d31fa858
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-26 14:09:52 +1100

    Time: 123 ms (54.93%), Space: 13.9 MB (93.93%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -8,18 +8,43 @@ class Solution:
             if total == target:
                 res.append(sub.copy())
                 return
-            if i >= len(candidates) or total > target or candidates[i] > target:
+            if i == len(candidates):
+                return
+            if  total > target:
                 return
             
             sub.append(candidates[i])
             dfs(i + 1, total + candidates[i])
-            
             sub.pop()
-            while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
+            while i + 1 < len(candidates) and candidates[i + 1] == candidates[i]:
                 i += 1
             dfs(i + 1, total)
-            
         
         dfs(0, 0)
-        
         return res
+        
+        
+        
+#         res = []
+#         sub = []
+#         candidates.sort()
+        
+#         def dfs(i, total):
+#             if total == target:
+#                 res.append(sub.copy())
+#                 return
+#             if i >= len(candidates) or total > target or candidates[i] > target:
+#                 return
+            
+#             sub.append(candidates[i])
+#             dfs(i + 1, total + candidates[i])
+            
+#             sub.pop()
+#             while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
+#                 i += 1
+#             dfs(i + 1, total)
+            
+        
+#         dfs(0, 0)
+        
+#         return res
