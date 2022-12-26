4bd6413a2f7b2e3e84c98b85c532d148f310115d
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-27 09:40:38 +1100

    Time: 687 ms (85.48%), Space: 30.1 MB (68.27%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -4,24 +4,55 @@ class Solution:
         sub = []
         
         def dfs(i):
-            if i >= len(s):
+            if i == len(s):
                 res.append(sub.copy())
                 return
             
             for j in range(i, len(s)):
-                if self.isPalin(s, i, j):
+                if self.isPalinD(i, j, s):
                     sub.append(s[i: j + 1])
-                    dfs(j +  1)
+                    dfs(j + 1)
                     sub.pop()
-                    
+            
         dfs(0)
-        
         return res
     
-    def isPalin(self, s, l, r):
-        while l < r:
-            if s[l] != s[r]:
+    def isPalinD(self, i, j, s):
+        while i <= j:
+            if s[i] != s[j]:
                 return False
-            l += 1
-            r -= 1
+            i += 1
+            j -= 1
         return True
+        
+        
+        
+        
+        
+        
+        
+#         res = []
+#         sub = []
+        
+#         def dfs(i):
+#             if i >= len(s):
+#                 res.append(sub.copy())
+#                 return
+            
+#             for j in range(i, len(s)):
+#                 if self.isPalin(s, i, j):
+#                     sub.append(s[i: j + 1])
+#                     dfs(j +  1)
+#                     sub.pop()
+                    
+#         dfs(0)
+        
+#         return res
+    
+#     def isPalin(self, s, l, r):
+#         while l < r:
+#             if s[l] != s[r]:
+#                 return False
+#             l += 1
+#             r -= 1
+#         return True
