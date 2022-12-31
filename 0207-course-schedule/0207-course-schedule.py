f6fecaa2963cb854b2ffbbceaa034e7a602045ff
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-31 11:45:46 +1100

    Time: 91 ms (98.97%), Space: 17.2 MB (24.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,55 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        crcToPre ={i: [] for i in range(numCourses)}
-        visited = set()
+        crcToPre = {i: [] for i in range(numCourses)}
+        cycle = set()
         
         for crc, pre in prerequisites:
             crcToPre[crc].append(pre)
         
-        def dfs(j):
-            if j in visited:
+        def dfs(i):
+            if i in cycle:
                 return False
-            if crcToPre[j] == []:
+            if not crcToPre[i]:
                 return True
-            
-            visited.add(j)
-            for pre in crcToPre[j]:
-                if not dfs(pre): return False
-            visited.remove(j)
-            crcToPre[j] = []
+            cycle.add(i)
+            for pre in crcToPre[i]:
+                if not dfs(pre):
+                    return False
+            cycle.remove(i)
+            crcToPre[i] = []
             return True
         
         for i in range(numCourses):
-            if not dfs(i): return False
-        
+            if not dfs(i):
+                return False
         return True
+        
+        
+        
+        
+        
+        
+#         crcToPre ={i: [] for i in range(numCourses)}
+#         cycle = set()
+        
+#         for crc, pre in prerequisites:
+#             crcToPre[crc].append(pre)
+        
+#         def dfs(j):
+#             if j in cycle:
+#                 return False
+#             if crcToPre[j] == []:
+#                 return True
+            
+#             cycle.add(j)
+#             for pre in crcToPre[j]:
+#                 if not dfs(pre): return False
+#             cycle.remove(j)
+#             crcToPre[j] = []
+#             return True
+        
+#         for i in range(numCourses):
+#             if not dfs(i): return False
+        
+#         return True
         
