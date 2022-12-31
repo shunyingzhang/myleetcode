dae620a397ba3f758c35652e65cc9a6f84f5301d
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-31 15:15:14 +1100

    Time: 60 ms (84.47%), Space: 14.4 MB (92.38%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -3,29 +3,55 @@ class Solution:
         par = [i for i in range(len(edges) + 1)]
         rank = [1] * (len(edges) + 1)
         
-        def findPar(n):
-            p = par[n]
+        def findpar(i):
+            p = par[i]
             while p != par[p]:
                 par[p] = par[par[p]]
                 p = par[p]
             return p
         
-        def union(n1, n2):
-            p1 = findPar(n1)
-            p2 = findPar(n2)
-            if p1 == p2:
-                return False
-            
-            if rank[p1] <= rank[p2]:
-                par[p1] = par[p2]
-                rank[p2] += rank[p1]
-            else:
-                par[p2] = par[p1]
-                rank[p1] += rank[p2]
-            
-            return True
-        
         for e1, e2 in edges:
-            if not union(e1, e2):
+            p1 = findpar(e1)
+            p2 = findpar(e2)
+            if p1 == p2:
                 return [e1, e2]
+            
+            if rank[p1] >= rank[p2]:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                par[p1] = p2
+                rank[p1] += rank[p2]
+        
+        
+        
+        
+#         par = [i for i in range(len(edges) + 1)]
+#         rank = [1] * (len(edges) + 1)
+        
+#         def findPar(n):
+#             p = par[n]
+#             while p != par[p]:
+#                 par[p] = par[par[p]]
+#                 p = par[p]
+#             return p
+        
+#         def union(n1, n2):
+#             p1 = findPar(n1)
+#             p2 = findPar(n2)
+#             if p1 == p2:
+#                 return False
+            
+#             if rank[p1] <= rank[p2]:
+#                 par[p1] = par[p2]
+#                 rank[p2] += rank[p1]
+#             else:
+#                 par[p2] = par[p1]
+#                 rank[p1] += rank[p2]
+            
+#             return True
+        
+#         for e1, e2 in edges:
+#             if not union(e1, e2):
+#                 return [e1, e2]
         
