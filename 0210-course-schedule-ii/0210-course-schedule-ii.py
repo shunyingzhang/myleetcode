8276fca2baa1cd20bf2e8177397bfc9c4a8f99c0
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2022-12-31 13:42:35 +1100

    Time: 101 ms (93.36%), Space: 17.7 MB (10.59%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -5,35 +5,73 @@ class Solution:
         visited = set()
         res = []
         
-        if prerequisites == []:
-            for i in range(numCourses):
-                res.append(i)
-            return res
-        
         for crc, pre in prerequisites:
             crcToPre[crc].append(pre)
-            
-        def dfs(j):
-            if j in visited:
-                return True
-            if j in cycle:
+        
+        def dfs(i):
+            if i in cycle:
                 return False
-
-            if crcToPre[j] == []:
-                visited.add(j)
-                res.append(j)
+            if i in visited:
                 return True
             
-            cycle.add(j)
-            for pre in crcToPre[j]:
-                if not dfs(pre): return False
-            cycle.remove(j)
-            # crcToPre[j] = []
-            res.append(j)
-            visited.add(j)
+            if not crcToPre[i]:
+                visited.add(i)
+                res.append(i)
+                return True
+            
+            cycle.add(i)
+            for pre in crcToPre[i]:
+                if not dfs(pre):
+                    return False
+            visited.add(i)
+            res.append(i)
+            cycle.remove(i)
             return True
         
         for i in range(numCourses):
-            if not dfs(i): return []
-        
+            if not dfs(i):
+                return []
         return res
+        
+        
+        
+        
+        
+        
+#         crcToPre = {i: [] for i in range(numCourses)}
+#         cycle = set()
+#         visited = set()
+#         res = []
+        
+#         if prerequisites == []:
+#             for i in range(numCourses):
+#                 res.append(i)
+#             return res
+        
+#         for crc, pre in prerequisites:
+#             crcToPre[crc].append(pre)
+            
+#         def dfs(j):
+#             if j in visited:
+#                 return True
+#             if j in cycle:
+#                 return False
+
+#             if crcToPre[j] == []:
+#                 visited.add(j)
+#                 res.append(j)
+#                 return True
+            
+#             cycle.add(j)
+#             for pre in crcToPre[j]:
+#                 if not dfs(pre): return False
+#             cycle.remove(j)
+#             # crcToPre[j] = []
+#             res.append(j)
+#             visited.add(j)
+#             return True
+        
+#         for i in range(numCourses):
+#             if not dfs(i): return []
+        
+#         return res
