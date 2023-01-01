f61a34d9cdf435242011a6a1a73936f7b615378c
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-01 11:35:34 +1100

    Time: 3325 ms (59.84%), Space: 174.8 MB (17.60%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,29 +1,57 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        N = len(points)
-        disPoint = {i: [] for i in range(N)}
+        n = len(points)
+        disp = {i: [] for i in range(n)}
         
-        for i in range(N):
-            for j in range(i + 1, N):
+        for i in range(n):
+            for j in range(i + 1, n):
                 dis = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
-                disPoint[i].append([dis, j])
-                disPoint[j].append([dis, i])
+                disp[i].append([dis, j])
+                disp[j].append([dis, i])
         
         res = 0
-        MinHeap = [[0, 0]]
         visited = set()
+        minheap = [[0, 0]]
         
-        while len(visited) < N:
-            cost, p = heapq.heappop(MinHeap)
+        while len(visited) < n:
+            cost, p = heapq.heappop(minheap)
             if p in visited:
                 continue
-            
             res += cost
             visited.add(p)
-            for dis, nei in disPoint[p]:
-                if nei not in visited:
-                    heapq.heappush(MinHeap, [dis, nei])
             
+            for dis, nei in disp[p]:
+                if nei not in visited:
+                    heapq.heappush(minheap, [dis, nei])
+                    
         return res
+        
+        
+        
+#         N = len(points)
+#         disPoint = {i: [] for i in range(N)}
+        
+#         for i in range(N):
+#             for j in range(i + 1, N):
+#                 dis = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+#                 disPoint[i].append([dis, j])
+#                 disPoint[j].append([dis, i])
+        
+#         res = 0
+#         MinHeap = [[0, 0]]
+#         visited = set()
+        
+#         while len(visited) < N:
+#             cost, p = heapq.heappop(MinHeap)
+#             if p in visited:
+#                 continue
+            
+#             res += cost
+#             visited.add(p)
+#             for dis, nei in disPoint[p]:
+#                 if nei not in visited:
+#                     heapq.heappush(MinHeap, [dis, nei])
+            
+#         return res
         
         
