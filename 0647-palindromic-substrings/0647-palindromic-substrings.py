8e62eebf8e466cab0da8601c4b009b3fa1924381
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-01 21:00:14 +1100

    Time: 117 ms (91.11%), Space: 13.8 MB (98.51%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,21 +1,44 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         res = 0
-        lenP = 0
         
         for i in range(len(s)):
+
             l = i
             r = i
-            while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
-            lenP += int((r - l) / 2)
-            
+                res += 1
+         
             l = i
             r = i + 1
-            while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
+ 
+            while l >= 0 and r < len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
-            lenP += int((r - l - 1) / 2)
+                res += 1
+        return res
+        
+        
+        
+        
+#         res = 0
+#         lenP = 0
+        
+#         for i in range(len(s)):
+#             l = i
+#             r = i
+#             while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
+#                 l -= 1
+#                 r += 1
+#             lenP += int((r - l) / 2)
             
-        return lenP
+#             l = i
+#             r = i + 1
+#             while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
+#                 l -= 1
+#                 r += 1
+#             lenP += int((r - l - 1) / 2)
+            
+#         return lenP
