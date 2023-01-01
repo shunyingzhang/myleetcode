faeff0caec84ae99025f3f49d082b01008b91148
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-01 20:42:53 +1100

    Time: 706 ms (77.40%), Space: 14.1 MB (31.63%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,25 +1,52 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
-        resLen = 0
+        res = ''
+        reslen = 0
         
         for i in range(len(s)):
             l = i
             r = i
-            while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
-                if (r - l + 1) > resLen:
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > reslen:
                     res = s[l: r + 1]
-                    resLen = r - l + 1
+                    reslen = r - l + 1
                 l -= 1
                 r += 1
-            
             l = i
             r = i + 1
-            while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
-                if (r - l + 1) > resLen:
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 > reslen:
                     res = s[l: r + 1]
-                    resLen = r - l + 1
+                    reslen = r - l + 1 
                 l -= 1
                 r += 1
+        
         return res
+        
+        
+        
+        
+        
+#         res = ""
+#         resLen = 0
+        
+#         for i in range(len(s)):
+#             l = i
+#             r = i
+#             while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
+#                 if (r - l + 1) > resLen:
+#                     res = s[l: r + 1]
+#                     resLen = r - l + 1
+#                 l -= 1
+#                 r += 1
+            
+#             l = i
+#             r = i + 1
+#             while l >= 0 and r <= len(s) - 1 and s[l] == s[r]:
+#                 if (r - l + 1) > resLen:
+#                     res = s[l: r + 1]
+#                     resLen = r - l + 1
+#                 l -= 1
+#                 r += 1
+#         return res
             
