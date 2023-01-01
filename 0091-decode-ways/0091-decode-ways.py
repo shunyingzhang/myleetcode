8e23a12783d832cb88248d328731af0eee9ef961
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-02 09:44:59 +1100

    Time: 33 ms (88.38%), Space: 13.9 MB (79.76%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,17 +1,32 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         dp = [1] * (len(s) + 1)
-        
-        
         for i in range(len(s) - 1, -1, -1):
-            if s[i] == "0":
+            if s[i] == '0':
                 dp[i] = 0
-            else:
-                dp[i] = dp[i + 1]
-                if (i + 1) < len(s):
-                    if s[i] == "1":
-                        dp[i] += dp[i + 2]
-                    elif s[i] == "2" and s[i + 1] in '0123456':
-                        dp[i] += dp[i + 2]  
+                continue
+            dp[i] = dp[i + 1]
+            if ((s[i] == '1' and i + 1 < len(s))
+               or (s[i] == '2' and i + 1 < len(s) 
+                   and int(s[i + 1]) <= 6)):
+                dp[i] += dp[i + 2]
+        
+        return dp[0] 
+        
+        
+        
+#         dp = [1] * (len(s) + 1)
+        
+        
+#         for i in range(len(s) - 1, -1, -1):
+#             if s[i] == "0":
+#                 dp[i] = 0
+#             else:
+#                 dp[i] = dp[i + 1]
+#                 if (i + 1) < len(s):
+#                     if s[i] == "1":
+#                         dp[i] += dp[i + 2]
+#                     elif s[i] == "2" and s[i + 1] in '0123456':
+#                         dp[i] += dp[i + 2]  
             
-        return dp[0]
+#         return dp[0]
