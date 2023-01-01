ee255c0d10e1af5f8077d6ee05f4026797c57d9b
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-01 15:03:06 +1100

    Time: 262 ms (54.48%), Space: 15 MB (90.22%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,17 +1,33 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        cost = [float('inf')] * n
+        cost = [float('inf')] * (n)
         cost[src] = 0
         
         for i in range(k + 1):
-            costTmp = cost.copy()
-            for s, d, p in flights:
+            tmp = cost.copy()
+            for s, d, c in flights:
                 if cost[s] == float('inf'):
                     continue
-                if cost[s] + p < costTmp[d]:
-                    costTmp[d] = cost[s] + p
-            cost = costTmp.copy()
+                if cost[s] + c < tmp[d]:
+                    tmp[d] = cost[s] + c
+            cost = tmp.copy()
+        
+        return cost[dst] if cost[dst] != float('inf') else -1
+        
+        
+        
+#         cost = [float('inf')] * n
+#         cost[src] = 0
+        
+#         for i in range(k + 1):
+#             costTmp = cost.copy()
+#             for s, d, p in flights:
+#                 if cost[s] == float('inf'):
+#                     continue
+#                 if cost[s] + p < costTmp[d]:
+#                     costTmp[d] = cost[s] + p
+#             cost = costTmp.copy()
                 
                 
-        return -1 if cost[dst] == float("inf") else cost[dst]
+#         return -1 if cost[dst] == float("inf") else cost[dst]
         
