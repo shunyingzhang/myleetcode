f97ce6b748835178ca3e6366943291269c2ad5e1
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-02 10:54:33 +1100

    Time: 83 ms (93.96%), Space: 14.4 MB (78.76%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,23 +1,34 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
+        res = max(nums)
+        maxn = 1
+        minn = 1
         
-        curMax = 1
-        curMin = 1
-        
-        res = nums[0]
-        
-        for c in nums:
-            tmp = curMax
-            curMax = max(c, c*curMax, c*curMin)
-            curMin = min(c, c*tmp, c*curMin)
-            res = max(res, curMax)
-            # maxMin[i].append(max(nums[i], 
-            #                      nums[i] * maxMin[i - 1][0], 
-            #                      nums[i] * maxMin[i - 1][1]))
-            # res = max(res, maxMin[i][0])
-            # maxMin[i].append(min(nums[i], nums[i] * maxMin[i - 1][0], nums[i] * maxMin[i - 1][1]))
+        for n in nums:
+            if n == 0:
+                maxn = 1
+                minn = 1
+                continue
+            tmp = maxn
+            maxn = max(tmp * n, minn * n, n)
+            minn = min(tmp * n, minn * n, n)
+            res = max(res, maxn)
         
         return res
+        
+#         if len(nums) == 1:
+#             return nums[0]
+        
+#         curMax = 1
+#         curMin = 1
+        
+#         res = nums[0]
+        
+#         for c in nums:
+#             tmp = curMax
+#             curMax = max(c, c*curMax, c*curMin)
+#             curMin = min(c, c*tmp, c*curMin)
+#             res = max(res, curMax)
+        
+#         return res
         
