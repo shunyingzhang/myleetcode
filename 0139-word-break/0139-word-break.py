a663671c1f8137bd5f53c910a4aa03e05c51160e
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-02 11:51:48 +1100

    Time: 47 ms (73.37%), Space: 13.9 MB (94.91%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -5,10 +5,31 @@ class Solution:
         
         for i in range(len(s) - 1, -1, -1):
             for w in wordDict:
-                if (i + len(w)) <= (len(s) + 1) and s[i: i + len(w)] == w:
+                if (i + len(w)) <= len(s) and s[i: i + len(w)] == w:
                     dp[i] = dp[i + len(w)]
                 if dp[i]:
                     break
-                
         return dp[0]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         dp = [False] * (len(s) + 1)
+#         dp[len(s)] = True
+        
+#         for i in range(len(s) - 1, -1, -1):
+#             for w in wordDict:
+#                 if (i + len(w)) <= (len(s) + 1) and s[i: i + len(w)] == w:
+#                     dp[i] = dp[i + len(w)]
+#                 if dp[i]:
+#                     break
+                
+#         return dp[0]
         
