6a9b824433efc6286855e9386346efb4f3d3a87e
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-02 16:21:11 +1100

    Time: 384 ms (95.28%), Space: 15.6 MB (62.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,25 +1,43 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         target = sum(nums)
-        if target % 2 !=  0:
+        if target % 2 != 0:
             return False
-        else:
-            target = target // 2
+        target = target // 2
         
-        rsdl = set()
-        rsdl.add(target)
+        if target in nums:
+            return True
         
+        hashset = set()
+        hashset.add(0)
         for n in nums:
-            if n in rsdl:
-                return True
-            nextrsdl = set()
-            for r in rsdl:
-                nextrsdl.add(r - n)
-                nextrsdl.add(r)
-            
-            rsdl = nextrsdl
-                
+            rsdl = hashset.copy()
+            for s in rsdl:
+                if n + s == target:
+                    return True
+                hashset.add(n + s)
         return False
+        
+#         target = sum(nums)
+#         if target % 2 !=  0:
+#             return False
+#         else:
+#             target = target // 2
+        
+#         rsdl = set()
+#         rsdl.add(target)
+        
+#         for n in nums:
+#             if n in rsdl:
+#                 return True
+#             nextrsdl = set()
+#             for r in rsdl:
+#                 nextrsdl.add(r - n)
+#                 nextrsdl.add(r)
+            
+#             rsdl = nextrsdl
+                
+#         return False
             
             
             
