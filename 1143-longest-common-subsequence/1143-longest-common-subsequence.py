1ded8204989ae780c243ad0c1aeac78a50a38694
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-05 10:53:19 +1100

    Time: 1445 ms (14.51%), Space: 13.8 MB (97.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,15 +1,34 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         row = [0] * (len(text2) + 1)
-        
         for i in range(len(text1) - 1, -1, -1):
-            newRow = [0] * (len(text2) + 1)
-            for j in range(len(text2) - 1, -1, -1):
-                if text1[i] == text2[j]:
-                    newRow[j] = 1 + row[j + 1]
+            tmp = row.copy()
+            for j in range(len(text2) -1, -1, -1):
+                if text1[i] != text2[j]:
+                    tmp[j] = max(row[j], tmp[j + 1])
                 else:
-                    newRow[j] = max(newRow[j + 1], row[j])
-            row = newRow
-                
+                    # tmp[j] = 1 + max(row[j], tmp[j + 1])
+                    tmp[j] = 1 + row[j + 1]
+            row = tmp
         
         return row[0]
+        
+        
+        
+        
+        
+        
+        
+#         row = [0] * (len(text2) + 1)
+        
+#         for i in range(len(text1) - 1, -1, -1):
+#             newRow = [0] * (len(text2) + 1)
+#             for j in range(len(text2) - 1, -1, -1):
+#                 if text1[i] == text2[j]:
+#                     newRow[j] = 1 + row[j + 1]
+#                 else:
+#                     newRow[j] = max(newRow[j + 1], row[j])
+#             row = newRow
+                
+        
+#         return row[0]
