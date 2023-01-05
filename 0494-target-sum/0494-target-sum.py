607b61c020df8fd9ae16548460c3af4bf0df632a
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-06 09:34:14 +1100

    Time: 1130 ms (13.94%), Space: 36 MB (27.94%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,19 +1,32 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        cache = {}
-        
+        dp = {}
         def dfs(i, t):
-            if i == len(nums):
-                if t == target:
-                    return 1
-                else:
-                    return 0
-            if (i, t) in cache:
-                return cache[(i, t)]
+            if (i, t) in dp:
+                return dp[(i, t)]
+            if i == len(nums) and t == target:
+                return 1
+            if i == len(nums) and t != target:
+                return 0
+            dp[(i, t)] = dfs(i + 1, t - nums[i]) + dfs(i + 1, t + nums[i])
+            return dp[(i, t)]
+        return dfs(0, 0)
+        
+        
+#         cache = {}
+        
+#         def dfs(i, t):
+#             if i == len(nums):
+#                 if t == target:
+#                     return 1
+#                 else:
+#                     return 0
+#             if (i, t) in cache:
+#                 return cache[(i, t)]
 
-            cache[(i, t)] = dfs(i + 1, t - nums[i]) + dfs(i + 1, t + nums[i])
-            return cache[(i, t)]
+#             cache[(i, t)] = dfs(i + 1, t - nums[i]) + dfs(i + 1, t + nums[i])
+#             return cache[(i, t)]
 
         
-        return dfs(0, 0)
+#         return dfs(0, 0)
         
