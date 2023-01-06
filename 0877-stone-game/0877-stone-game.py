b95228cfa5ae03e871ec083862e3849eb3636d13
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-06 20:24:24 +1100

    Time: 1711 ms (11.22%), Space: 115.1 MB (23.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,20 +1,18 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         target = sum(piles) // 2
-        piles = deque(piles)
+        dp = {}
+        def dfs(l, r):
+            if (l, r) in dp:
+                return dp[(l, r)]
+            if l > r:
+                return 0
+            even = (r - l) % 2
+            if even:
+                dp[(l, r)] = max(piles[l] + dfs(l + 1, r), piles[r] + dfs(l, r - 1))
+            else:
+                dp[(l, r)] = max(dfs(l + 1, r), dfs(l, r - 1))
+            return dp[(l, r)]
         
-        def dfs(s):
-            if not piles and s > target:
-                return True
-            if not piles and s <= target:
-                return False
-            pile = piles.popleft()
-            if dfs(s + pile):
-                return True
-            piles.pushleft(pile)
-            pile = piles.pop()
-            if dfs(s + pile):
-                return True
-            return False
-        return dfs(0)
+        return dfs(0, len(piles) - 1) > target
         
