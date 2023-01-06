f5a661bf37359ee08e2d393687d839080ce46248
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-06 21:18:52 +1100

    Time: 3671 ms (26.76%), Space: 58.8 MB (37.14%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,14 +1,24 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         dp = {}
-        def dfs(i, even):
-            if (i, even) in dp:
-                return dp[(i, even)]
-            if i == len(nums):
-                return 0
-            if even:
-                dp[(i, even)] = max(nums[i] + dfs(i + 1, False), dfs(i + 1, even))
-            else:
-                dp[(i, even)] = max(-nums[i] + dfs(i + 1, True), dfs(i + 1, even))
-            return dp[(i, even)]
-        return dfs(0, True)
+        dp[(len(nums), False)] = 0
+        dp[(len(nums), True)] = 0
+        
+        for i in range(len(nums) - 1, -1, -1):
+            dp[(i, True)] = max(nums[i] + dp[(i + 1, False)], dp[(i + 1, True)])
+            dp[(i, False)] = max(-nums[i] + dp[(i + 1, True)], dp[(i + 1, False)])
+        
+        return dp[(0, True)]
+        
+        # dp = {}
+        # def dfs(i, even):
+        #     if (i, even) in dp:
+        #         return dp[(i, even)]
+        #     if i == len(nums):
+        #         return 0
+        #     if even:
+        #         dp[(i, even)] = max(nums[i] + dfs(i + 1, False), dfs(i + 1, even))
+        #     else:
+        #         dp[(i, even)] = max(-nums[i] + dfs(i + 1, True), dfs(i + 1, even))
+        #     return dp[(i, even)]
+        # return dfs(0, True)
