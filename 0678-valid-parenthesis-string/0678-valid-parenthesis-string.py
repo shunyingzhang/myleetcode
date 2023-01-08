7765db74b4afe9c32cc052a490b755ce0ecb7bef
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-08 20:00:33 +1100

    Time: 41 ms (63.41%), Space: 14.6 MB (12.68%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -1,45 +1,42 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         dp = {}
-        
         def dfs(i, left):
+            if (i, left) in dp:
+                return dp[(i, left)]
             if i == len(s):
                 return left == 0
             if left < 0:
                 return False
-            if (i, left) in dp:
-                return dp[(i, left)]
-            
             if s[i] == '(':
                 dp[(i, left)] = dfs(i + 1, left + 1)
-            if s[i] == ')':
+            elif s[i] == ')':
                 dp[(i, left)] = dfs(i + 1, left - 1)
-            if s[i] == '*':
-                dp[(i, left)] = (dfs(i + 1, left + 1) or dfs(i + 1, left - 1) 
-                or dfs(i + 1, left))
+            else:
+                dp[(i, left)] = dfs(i + 1, left + 1) or dfs(i + 1, left - 1) or dfs(i + 1, left)
             return dp[(i, left)]
-                      
-                      
         return dfs(0, 0)
         
+#         dp = {}
         
-#         lMax = 0
-#         lMin = 0
-        
-#         for c in s:
-#             if c == '(':
-#                 lMax += 1
-#                 lMin += 1
-#             elif c == '*':
-#                 lMax += 1
-#                 lMin -= 1
-#             else:
-#                 lMax -= 1
-#                 lMin -= 1
-#             if lMax < 0:
+#         def dfs(i, left):
+#             if i == len(s):
+#                 return left == 0
+#             if left < 0:
 #                 return False
-#             if lMin < 0:
-#                 lMin = 0
+#             if (i, left) in dp:
+#                 return dp[(i, left)]
+            
+#             if s[i] == '(':
+#                 dp[(i, left)] = dfs(i + 1, left + 1)
+#             if s[i] == ')':
+#                 dp[(i, left)] = dfs(i + 1, left - 1)
+#             if s[i] == '*':
+#                 dp[(i, left)] = (dfs(i + 1, left + 1) or dfs(i + 1, left - 1) 
+#                 or dfs(i + 1, left))
+#             return dp[(i, left)]
+                      
+                      
+#         return dfs(0, 0)
         
-#         return lMin == 0
         
