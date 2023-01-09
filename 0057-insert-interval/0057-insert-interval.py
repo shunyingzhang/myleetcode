279fbfd852d7f8f9a4aafed89791e0d00f86e84c
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-10 10:50:43 +1100

    Time: 86 ms (80.78%), Space: 17.4 MB (53.48%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,19 +1,33 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        n = len(intervals) 
         res = []
-        for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
+        for i, intv in enumerate(intervals):
+            if intv[1] < newInterval[0]:
+                res.append(intv)
+            elif intv[0]> newInterval[1]:
                 res.append(newInterval)
-                return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
+                return res + intervals[i:]      
             else:
-                newInterval[0] = min(newInterval[0], intervals[i][0])
-                newInterval[1] = max(newInterval[1], intervals[i][1])
-            
+                newInterval[0] = min(intv[0], newInterval[0])
+                newInterval[1] = max(intv[1], newInterval[1])
         res.append(newInterval)
         return res
+        
+        
+#         n = len(intervals) 
+#         res = []
+#         for i in range(len(intervals)):
+#             if newInterval[1] < intervals[i][0]:
+#                 res.append(newInterval)
+#                 return res + intervals[i:]
+#             elif newInterval[0] > intervals[i][1]:
+#                 res.append(intervals[i])
+#             else:
+#                 newInterval[0] = min(newInterval[0], intervals[i][0])
+#                 newInterval[1] = max(newInterval[1], intervals[i][1])
+            
+#         res.append(newInterval)
+#         return res
                 
             
                 
