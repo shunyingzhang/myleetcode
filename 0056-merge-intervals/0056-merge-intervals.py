0d36eb5b0b433646925d000a496f0dd69f7ff237
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-10 11:00:59 +1100

    Time: 145 ms (95.25%), Space: 18.1 MB (53.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,18 +2,34 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
         res = []
-        interNew = intervals[0]
-        
-        for i in range(len(intervals)):
-            if interNew[1] < intervals[i][0]:
-                res.append(interNew)
-                interNew = intervals[i]
-            elif interNew[0] > intervals[i][1]:
-                res.append(intervals[i])
+        for i, intv in enumerate(intervals):
+            if i == 0:
+                new = intv
+                continue
+            if intv[0] <= new[1]:
+                new[0] = min(new[0], intv[0])
+                new[1] = max(new[1], intv[1])
             else:
-                interNew[0] = min(interNew[0], intervals[i][0])
-                interNew[1] = max(interNew[1], intervals[i][1])
-            
-        res.append(interNew)
+                res.append(new)
+                new = intv
+        res.append(new)    
         return res
+        
+        
+#         intervals.sort()
+#         res = []
+#         interNew = intervals[0]
+        
+#         for i in range(len(intervals)):
+#             if interNew[1] < intervals[i][0]:
+#                 res.append(interNew)
+#                 interNew = intervals[i]
+#             elif interNew[0] > intervals[i][1]:
+#                 res.append(intervals[i])
+#             else:
+#                 interNew[0] = min(interNew[0], intervals[i][0])
+#                 interNew[1] = max(interNew[1], intervals[i][1])
+            
+#         res.append(interNew)
+#         return res
         
