8ffa14229d20d5055b29d89a7efbee2cf859f837
shunyingzhang <111849080+shunyingzhang@users.noreply.github.com>
2023-01-17 11:41:18 +1100

    Time: 36 ms (72.60%), Space: 13.8 MB (45.49%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -3,7 +3,22 @@ class Solution:
         res = 0
         for i in range(32):
             a = (n >> i) & 1
-            res = res | (a << 31 - i)
+            res = (a << (31 - i)) | res
         
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = 0
+#         for i in range(32):
+#             a = (n >> i) & 1
+#             res = res | (a << 31 - i)
+        
+#         return res
         
